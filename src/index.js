d5381ff47d89aa6168de954b6ec266614331ef57
SuchitaGhadge <suchita8494.sg@gmail.com>
2022-08-16 16:17:42 +0530

    img in webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from './App'
-
+import Logo from './logo.svg'
 const root = document.getElementById('root');
 root.innerHTML = `
     <h1> Learning Web tooling today </h1>
+    <img src="${Logo}" alt="Build icon" />
     ${App({name : "Suchita"})}
 `
